f1a6c89dd2e9baf7d81bb531b75c0846b7f019ef
AlberCarri <alber-99@hotmail.es>
2020-05-20 13:31:30 +0200

    APIs

---
+++ API.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+from Discos import Discos
+
+#Voy a comprobar que el puerto funciona y que se ha activado de manera correcta
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({'response': 'pong!'})
+
+
+#ahora empiezo con la creación de la API de Discos
+#en primer lugar hago que se muestren todos los discos que se han registrado
+@app.route('/Discos')
+def getDiscos():
+    return jsonify({'Discos': Discos})
+
+#Ahora lo que quiero es que muestre los datos de un disco, según su titulo
+@app.route('/Discos/<string:product_name>')
+def getDisco(product_name):
+    for Disco in Discos:
+        if Disco['titulo'] == product_name.lower():
+            DiscosFound=Disco
+    print(type(DiscosFound))
+    if (len(DiscosFound) > 0):
+        return jsonify({'Disco': DiscosFound})
+    return jsonify({'message': 'Producto no encontrado'})
+
+#En esta ocasión lo que se busca, es mostrar los discos que se den en un año determinado
+@app.route('/Discos/<string:product_anyo>')
+def getAnyo(product_anyo):
+
+    for anyo in Discos:
+        if anyo['año_publicacion'] == product_anyo.lower():
+            anyoFound=anyo
+    print(type(anyoFound))
+    if(len(anyoFound) > 0):
+        return jsonify({'anyo': anyoFound})
+    return jsonify({'message': 'Producto no encontrado'})
+
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000)
---
+++ Discos.py
@@ -0,0 +1,7 @@
+Discos = [
+    {'titulo': 'Corazon Partio', 'artista': 'Alejandro Sanz', 'pais': 'España', 'genero': 'Pop', 'anyo_publicacion': 1997},
+    {'titulo': 'Diamonds', 'artista': 'Rihanna', 'pais': 'EEUU', 'genero': 'Pop', 'anyo_publicacion': 2012},
+    {'titulo': 'Pizza', 'artista': 'Martin Garrix', 'pais': 'Paises Bajos', 'genero': 'House', 'anyo_publicacion': 2017},
+    {'titulo': 'Lo Siento', 'artista': 'Beret', 'pais': 'España', 'genero': 'Pop', 'anyo_publicacion': 2018},
+    {'titulo': 'Mi Persona Favorita', 'artista': 'Alejandro Sanz', 'pais': 'España', 'genero': 'Pop', 'anyo_publicacion': 2019}
+]
